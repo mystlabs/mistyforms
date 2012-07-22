fa4cad1e8a74df3b71716b94e63bc04e69b89c30
Giacomo Gatelli <arthens@gmail.com>
2012-07-22 18:24:15 +1000

    Remove reference to old plugins

--- src/MistyForms/smarty_plugins/function.intfield.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Mist\Form\Input\IntField;
-use Mist\Form\FormBlock;
-
-function smarty_function_intfield( $params, $smarty )
-{
-	return FormBlock::addInput( new IntField( $params, $smarty ) );
-}
